eadb119afd76c45757c9b6b1825156dc1cad3555
Ivan Romero <isromeroc@academia.usbbog.edu.co>
2026-02-13 20:49:27 -0500

    Este si funciona

--- game.js
+++ game.js
@@ -1,2 +1,19 @@
-console.log("Primer cambio")
-console.log("Segundo Cambio")
+let n = Math.floor(Math.random() * 10) + 1;
+console.log("Número a adivinar:", n);
+
+for (let i = 0; i < 11; i++) {
+
+    let num = Number(prompt("Ingresa un número del 1 al 10"));
+
+    if (num == n) {
+        console.log(" Ganaste");
+        break;
+    } 
+    else if (Math.abs(n - num) <= 2) {
+        console.log(" Caliente");
+    } 
+    else {
+        console.log("Frío");
+    }
+
+}
